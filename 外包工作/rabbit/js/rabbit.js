9297f30ce5b595a1cf2790404aec5629b65acf26
CJ0423 <b09785157888@gmail.com>
2023-02-10 07:16:52 +0800

    l

--- 外包工作/rabbit/js/rabbit.js
+++ 外包工作/rabbit/js/rabbit.js
@@ -1,60 +1,65 @@
 // 開始按鈕
-const startBtn=document.querySelector(".btn-start")
-const backgroun=document.querySelector(".all")
-const station=document.querySelector(".station-img")
-const rabbits=document.querySelector('.rabbits')
-let level=0
-function start(){
-    level=1
+const startBtn = document.querySelector(".btn-start")
+const backgroun = document.querySelector(".all")
+const station = document.querySelector(".station-img")
+const rabbits = document.querySelector('.rabbits')
+let level = 0
+function start() {
+    level = 1
     console.log(level)
-    startBtn.style="visibility:hidden"
+    startBtn.style = "visibility:hidden"
     // 進入到遊戲中
     document.dispatchEvent(new Event("gameing"))
 }
-function chat(){
-if(level==10){return}
+function chat() {
+    if (level == 10) { return }
     level++
     console.log(level)
 }
-startBtn.addEventListener("click",start)
+startBtn.addEventListener("click", start)
 //測試用功能
-const chatBtn=document.querySelector(".chat-btn")
-chatBtn.addEventListener("click",chat)
+const chatBtn = document.querySelector(".chat-btn")
+chatBtn.addEventListener("click", chat)
 // 物件拖拉功能
 
 // 進入到遊戲中
-document.addEventListener("gameing",gameing)
-const radditColor=["b","g","p","r","y"]
-const radditColorNumber=[0,1,2,3,4,5]
-function gameing(){
-    rabbits.innerHTML=""
-    rabbits.style="left:100%"
-    if (level<=3){
-          // 三節車
-    rabbits.innerHTML=`<div class="rabbit-car">
+document.addEventListener("gameing", gameing)
+const radditColor = ["b", "g", "p", "r", "y"]
+const radditColorNumber = [0, 1, 2, 3, 4, 5]
+function gameing() {
+    rabbits.innerHTML = ""
+    rabbits.style = "left:100%"
+    if (level <= 3) {
+        // 三節車
+        rabbits.innerHTML = `<div class="rabbit-car">
     <div class="rabbit"></div>
     <div class="car"></div>
 </div>`}
 
-    else if(level<=5){
+    else if (level <= 5) {
         // 四和五 四節車
     }
-    else if(level<=8){
+    else if (level <= 8) {
         // 六到八 後進先出
     }
-    else if(level<=10){
+    else if (level <= 10) {
         // 九到十 要改地圖
     }
     document.dispatchEvent(new Event("moveCar"))
 }
 // 移動車車
-document.addEventListener("moveCar",moveCar)
-function moveCar(){
+document.addEventListener("moveCar", moveCar)
+function moveCar() {
+   let baseleft=100
     setInterval(
-        ( () => console.log("Hi") ), 1000);
+        (() => {
+            console.log(baseleft)
+            baseleft = baseleft- 5
+            rabbits.style.left = baseleft + "%";
+        }), 1000);
 
 
-    rabbits.style="left:65%"
+    // rabbits.style = "left:65%"
 }
 
 
