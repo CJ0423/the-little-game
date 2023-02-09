383777f0895ac35ab6dfded8682682c449e6e380
CJ0423 <b09785157888@gmail.com>
2023-02-09 15:42:10 +0800

    ˇ

--- 卡比變裝秀/kirby.js
+++ 卡比變裝秀/kirby.js
@@ -1,29 +1,28 @@
-const dragElement=document.querySelector(".glass")
+let dragElement=document.querySelectorAll(".item")
+
 let isMove=false
 
 // 每次點擊時都會進入到move中點擊1下啟動跟隨滑鼠移動再點一次則是取消跟隨滑鼠函數
 function move(e){
     isMove=!isMove
     if(isMove){                
-        dragElement.addEventListener("mousemove", followMouse)
+        this.addEventListener("mousemove", followMouse)
     }
     else{
-        console.log("hi")
-
-        dragElement.removeEventListener("mousemove",followMouse)
+        this.removeEventListener("mousemove",followMouse)
     }
 }
 
 function followMouse(e){
     // 點擊後確保滑鼠在正中間
-    console.log("瀏覽器位置"+e.clientY)
-    console.log("圖片位置"+dragElement.style.top)
-    dragElement.style.left=`${e.clientX - dragElement.offsetWidth/2}px`
+    this.style.left=`${e.clientX - this.offsetWidth/2}px`
 
-    dragElement.style.top=`${e.clientY - dragElement.offsetHeight/2}px`
+    this.style.top=`${e.clientY - this.offsetHeight/2}px`
     // requestAnimationFrame(followMouse)
 }
-dragElement.addEventListener("click",move)
+dragElement.forEach(element => {
+    element.addEventListener("click",move)
+});
 
 // clientX表示整個視窗中X軸的位置 由左往右從0開始
 // clientY表示整個視窗中Y軸的位置 由上往下從0開始
