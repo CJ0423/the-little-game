476867512a078711b0d236172f39ef9707b12f54
CJ0423 <b09785157888@gmail.com>
2023-03-20 15:27:50 +0800

    ㄜ

---
+++ 家教教材/新手教學/lesson01/newfile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>這是我的第一個網頁</title>
+</head>
+<body>
+    <h1>把資料寫在網 <?php
+
+
+$hi=1
+    echo "hi"?>頁上，這樣你就可以看到囉</h1>
+ 
+</body>
+</html>
